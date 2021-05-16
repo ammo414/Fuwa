15f911b656f5ff290e53873c48976829a4a71485
ammo414 <58457535+ammo414@users.noreply.github.com>
2021-05-15 22:05:55 -0400

    Add files via upload

---
+++ main.py
@@ -0,0 +1,53 @@
+from bs4 import BeautifulSoup as bs
+import requests
+import discord
+from jikanpy import Jikan
+import re
+
+
+def vrvmalurl(vrvlink):
+    jikan = Jikan()
+    page = requests.get(vrvlink)
+    soup = bs(page.content, 'html.parser')
+    title = str(soup.find_all('title'))[8:-9]
+    mallink = jikan.search('anime', title)['results'][0]['url']
+    return mallink
+
+
+def funimalurl(funilink):
+    jikan = Jikan()
+    title = funilink.replace('https://www.funimation.com/shows/', '').replace('-', ' ')[:-1].strip()
+    mallink = jikan.search('anime', title)['results'][0]['url']
+    return mallink
+
+
+def netflixmalurl(netflixlink):
+    url = re.findall('https:.*$', netflixlink)
+    jikan = Jikan()
+    page = requests.get(url[0])
+    soup = bs(page.content, 'html.parser')
+    title = str(soup.find_all('title'))[8:-19]
+    mallink = jikan.search('anime', title)['results'][0]['url']
+    return mallink
+
+
+client = discord.Client()
+
+
+@client.event
+async def on_ready():
+    pass
+
+
+@client.event
+async def on_message(message):
+    if 'vrv' in message.content:
+        await message.channel.send(vrvmalurl(message.content))
+    elif 'funimation' in message.content:
+        await message.channel.send(funimalurl(message.content))
+    elif 'netflix' in message.content:
+        await message.channel.send(netflixmalurl(message.content))
+
+
+client.run('ODMwNDg5OTUzMTY3MjEyNTY0.YHHcCg.wADQvjbM9xpp9I1YbHOJ-a46My8')
+
